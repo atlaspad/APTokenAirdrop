08c9175eb66a673bad9930a27ad62f57bfe61b6b
berk cicek <90208101+berkcicekk@users.noreply.github.com>
2024-04-29 17:36:27 +0300

    Update APAirdrop.sol

--- APAirdrop.sol
+++ APAirdrop.sol
@@ -1,33 +1,101 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.20;
+pragma solidity ^0.8.25;
 
-import "@openzeppelin/contracts/utils/cryptography/MerkleProof.sol";
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
+import {IERC20, SafeERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import {MerkleProof} from "@openzeppelin/contracts/utils/cryptography/MerkleProof.sol";
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 
+// Hata durumlarını tanımlamak için kullanılan özel hata mesajları
+error StartInPast(); // Başlangıç zamanı geçmiş durumda
+error AlreadyClaimed(); // Daha önce talep edilmiş
+error InvalidProof(); // Geçersiz Merkle ağacı ispatı
+error NotInClaimWindow(); // Talep penceresi dışında
 
-contract AirDrop is ERC20("AirDrop", "AD") {
-    bytes32 public immutable root;
-    uint256 public immutable rewardAmount;
-    mapping(address => bool) claimed;
-    Atlaspad atlaspadContract;
+contract Airdrop is Ownable {
+    using SafeERC20 for IERC20;
 
-    constructor(bytes32 _root, uint256 _rewardAmount) {
+    // Değişkenlerin tanımlanması
+    address public immutable token; // Dağıtılacak tokenın adresi
+    bytes32 public root; // Merkle ağacının kökü
+    uint256 public start; // Dağıtımın başlangıç zamanı
+    uint256 public duration; // Dağıtımın süresi
+    uint256 public distributed; // Dağıtılan toplam token miktarı
+    mapping(address => uint256) public history; // Adreslerin dağıtımdan aldığı miktarlar
+    uint256 public count; // Toplam adres sayısı
+    mapping(uint256 => uint256) private claimed; // Talep edilmiş adreslerin kaydedildiği bit dizisi
+
+    event Claimed(uint256 index, address account, uint256 amount);
+
+    // Kurucu fonksiyon, sözleşme oluşturulurken çağrılır
+    constructor(address _token, address _owner) Ownable(_owner) {
+        token = _token;
+    }
+
+    // Güncelleme fonksiyonu, sahibin kök ve zamanı güncellemesine izin verir
+    function update(
+        bytes32 _root,
+        uint256 _start,
+        uint256 _duration
+    ) public onlyOwner {
+        if (_start <= block.timestamp) revert StartInPast(); // Başlangıç zamanı geçmiş mi kontrolü
         root = _root;
-        rewardAmount = _rewardAmount;
-        Atlaspad atlaspadContract = Atlaspad(contractAddress);
+        start = _start;
+        duration = _duration;
     }
 
+    // Talep edilmiş bir indeksin işaretlenip işaretlenmediğini kontrol eder
+    function isClaimed(uint256 index) public view returns (bool) {
+        uint256 rowIndex = index / 256;
+        uint256 colIndex = index % 256;
+        uint256 row = claimed[rowIndex];
+        uint256 mask = (1 << colIndex);
+        return row & mask == mask;
+    }
 
-    function claim(bytes32[] calldata _proof) external {
-        require(!claimed[msg.sender], "Already claimed airdrop");
-        claimed[msg.sender] = true;
-        bytes32 _leaf = keccak256(abi.encodePacked(msg.sender));
-        require(
-            MerkleProof.verify(_proof, root, _leaf),
-            "Incorrect merkle proof"
+    // İşaretlenmiş bir indeksi ayarlar
+    function _setClaimed(uint256 index) private {
+        uint256 rowIndex = index / 256;
+        uint256 colIndex = index % 256;
+        claimed[rowIndex] = claimed[rowIndex] | (1 << colIndex);
+    }
+
+    // Hava atışı tokenlarını talep etmek için kullanılan fonksiyon
+    function claim(
+        uint256 index,
+        uint256 amount,
+        bytes32[] calldata proofs
+    ) public {
+        if (isClaimed(index)) revert AlreadyClaimed(); // Daha önce talep edilmiş mi kontrolü
+        if (start == 0 || block.timestamp < start) revert NotInClaimWindow(); // Talep penceresi kontrolü
+        if (block.timestamp > start + duration) revert NotInClaimWindow(); // Talep penceresi kontrolü
+        if (distributed + amount > IERC20(token).balanceOf(address(this)))
+            revert NotInClaimWindow(); // Dağıtımın bakiyesi kontrolü
+
+        // Merkle ağacı ispatını doğrulama
+        bytes32 leaf = keccak256(
+            abi.encode(index, msg.sender, amount)
         );
-        _mint(msg.sender, rewardAmount);
+        if (!MerkleProof.verify(proofs, root, leaf)) revert InvalidProof();
+
+        // Talep edildi olarak işaretle ve tokenları gönder
+        _setClaimed(index);
+        IERC20(token).safeTransfer(msg.sender, amount);
+
+        // İlk defa talep edenlerin sayısını artır
+        if (history[msg.sender] == 0) {
+            count++;
+        }
+        history[msg.sender] += amount;
+        distributed += amount;
+        emit Claimed(index, msg.sender, amount);
     }
+
+    // Fonksiyon airdrop süresi dolana kadar tokenları geri çekmeyi sağlar
+    // function withdraw() external onlyOwner {
+    //     if (block.timestamp < start + duration) revert NoWithdrawDuringClaim();
+    //     IERC20(token).safeTransfer(
+    //         msg.sender,
+    //         IERC20(token).balanceOf(address(this))
+    //     );
+    // }
 }
