090f5be46e40acada000e3690372509d32786123
virjil <158029357+virjilakrum@users.noreply.github.com>
2024-05-04 16:07:31 +0300

    import ./APtoken
    This is our Main Contract file which you need to deploy. You can customize contract as per your requirement.
    migrations/1_initial_migration.js
    
    Our Smart contract takes 1 constructor parameter, You need to pass Token Contract Address which you want to Airdrop. That you can specify in this file.
    src/airdrop.csv
    
    List of Addresses you want to airdrop tokens to. (Sample file included, you can also download list from bscscan,etherscan)
    src/airdrop.js
    
    This is a node script to interect with deployed airdrop smart contract. it reads list of airdrop beneficiaries and airdrops token in batch size you have specified. in init3 function you need to specify deployed smart contract address.
    .env
    
    File which contains 12 secret mnemonic phrases of your hd wallet in SEED_PHRASE variable.

--- airdrop.sol
+++ airdrop.sol
@@ -1,28 +1,61 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.20;
+pragma solidity >=0.7.0;
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/math/SafeMath.sol";
+import "./APToken.sol";
 
-import "@openzeppelin/contracts/utils/cryptography/MerkleProof.sol";
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import "hardhat/console.sol";
 
-contract AirDrop is ERC20("AirDrop", "AD") {
-    bytes32 public immutable root;
-    uint256 public immutable rewardAmount;
-    mapping(address => bool) claimed;
+contract Airdrop is Ownable {
+    using SafeMath for uint;
 
-    constructor(bytes32 _root, uint256 _rewardAmount) {
-        root = _root;
-        rewardAmount = _rewardAmount;
+    address public tokenAddr;
+
+    event EtherTransfer(address beneficiary, uint amount);
+
+    constructor(address _tokenAddr) public {
+        tokenAddr = _tokenAddr;
     }
 
-    function claim(bytes32[] calldata _proof) external {
-        require(!claimed[msg.sender], "Already claimed air drop");
-        claimed[msg.sender] = true;
-        bytes32 _leaf = keccak256(abi.encodePacked(msg.sender));
-        require(
-            MerkleProof.verify(_proof, root, _leaf),
-            "Incorrect merkle proof"
-        );
-        _mint(msg.sender, rewardAmount);
+    function dropTokens(address[] memory _recipients, uint256[] memory _amount) public onlyOwner returns (bool) {
+       
+        for (uint i = 0; i < _recipients.length; i++) {
+            require(_recipients[i] != address(0));
+            require(Token(tokenAddr).transfer(_recipients[i], _amount[i]));
+        }
+
+        return true;
+    }
+
+    function dropEther(address[] memory _recipients, uint256[] memory _amount) public payable onlyOwner returns (bool) {
+        uint total = 0;
+
+        for(uint j = 0; j < _amount.length; j++) {
+            total = total.add(_amount[j]);
+        }
+
+        require(total <= msg.value);
+        require(_recipients.length == _amount.length);
+
+
+        for (uint i = 0; i < _recipients.length; i++) {
+            require(_recipients[i] != address(0));
+
+            payable(_recipients[i]).transfer(_amount[i]);
+
+            emit EtherTransfer(_recipients[i], _amount[i]);
+        }
+
+        return true;
+    }
+
+    function updateTokenAddress(address newTokenAddr) public onlyOwner {
+        tokenAddr = newTokenAddr;
+    }
+
+    function withdrawTokens(address beneficiary) public onlyOwner {
+        require(Token(tokenAddr).transfer(beneficiary, Token(tokenAddr).balanceOf(address(this))));
+    }
+
+    function withdrawEther(address payable beneficiary) public onlyOwner {
+        beneficiary.transfer(address(this).balance);
     }
 }
