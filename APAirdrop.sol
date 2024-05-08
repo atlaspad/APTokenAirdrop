4fa5b71db63f8eb0a6bf6ccf6eaf5e853f592b2a
älkın <asesenep15@gmail.com>
2024-05-08 16:39:10 +0300

    fix airdrrop version

--- APAirdrop.sol
+++ APAirdrop.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.25;
+pragma solidity ^0.8.19;
 
 import {IERC20, SafeERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import {MerkleProof} from "@openzeppelin/contracts/utils/cryptography/MerkleProof.sol";
